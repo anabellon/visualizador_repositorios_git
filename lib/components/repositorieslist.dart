24ff322804a0af4269ece5c47d37165c8ae1b7ca
Ana Paula Bellon <anapaulabellondev@gmail.com>
2023-03-17 13:01:26 -0400

    wip

--- lib/components/repositorieslist.dart
+++ lib/components/repositorieslist.dart
@@ -1,27 +1,24 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-/*
-  !Em construção!
-  List Build para retornar os repositorios em Cards no 'result.dart'
+//   !Em construção!
+//   List Build para retornar os repositorios em Cards no 'result.dart'
 
-class RepositoriesList extends StatelessWidget {
-  const RepositoriesList(
-      {super.key,
-      required this.title,
-      required this.description,
-      required this.link});
+// class RepositoriesList extends StatelessWidget {
+//   const RepositoriesList(
+//       {super.key,
+//       required this.title,
+//       required this.description,
+//       required this.link});
 
-  final String title;
-  final String description;
-  final String link;
+//   final String title;
+//   final String description;
+//   final String link;
 
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(title),
-      subtitle: Text(description),
-    );
-  }
-}
-
-*/
+//   @override
+//   Widget build(BuildContext context) {
+//     return ListTile(
+//       title: Text(title),
+//       subtitle: Text(description),
+//     );
+//   }
+// }
--- lib/services/apiservice.dart
+++ lib/services/apiservice.dart
@@ -1,15 +1,18 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:visualizador_repositorios_git/model/repositoriesmodel.dart';
 
 class ApiService {
   Future<void> get() async {
     Response response;
-    Dio dio = new Dio();
+    Dio dio = Dio();
     try {
       response = await dio.get("https://api.github.com/users/anabellon/repos");
-      //var listRepositories = (response.data as List)
 
-      debugPrint("Foi? ${response.data}");
+      debugPrint(
+          "Foi? ${response.data[0]['id']}"); //pegando o id do objeto 0 do vetor
     } on DioError catch (err) {
       debugPrint("Deu erro kkkcry ${err.response!.statusCode}");
     }
